bc1773aff564903cc87d8f5efa52f4db28d7a2a2
MuhammadSafyanTariq <mstdevops786@gmail.com>
2023-10-03 19:18:46 +0500

    Done with all

--- lib/chat/homepage.dart
+++ lib/chat/homepage.dart
@@ -25,9 +25,12 @@ class _MyHomePageState extends State<MyHomePage> {
   bool open = false;
   @override
   Widget build(BuildContext context) {
+    double W = MediaQuery.of(context).size.width;
+    double H = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.indigo.shade400,
+        backgroundColor: Colors.black,
         title: Text('Messages'),
         elevation: 0,
         centerTitle: true,
@@ -47,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      backgroundColor: Colors.indigo.shade400,
+      backgroundColor: Colors.black,
       // drawer: ChatWidgets.drawer(context),
       body: SafeArea(
         child: Stack(
@@ -59,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Container(
                   margin: EdgeInsets.all(0),
                   child: Container(
-                    color: Colors.indigo.shade400,
+                    color: Colors.black,
                     padding: const EdgeInsets.all(8),
                     height: 160,
                     child: Column(
@@ -153,96 +156,135 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 10),
-                    decoration: Styles.friendsBox(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 20),
-                          child: Text(
-                            'Contacts',
-                            style: Styles.h1().copyWith(color: Colors.indigo),
+                  child: Center(
+                    child: Container(
+                      padding: EdgeInsets.all(6),
+                      constraints: BoxConstraints(
+                        minWidth: W * 0.95,
+                        maxWidth: W * 0.95,
+                        minHeight: H * 0.8,
+                        maxHeight: H * 0.8,
+                      ),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                          style: BorderStyle.solid,
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            (20),
                           ),
                         ),
-                        Expanded(
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 12.0),
-                            child: StreamBuilder(
-                                stream:
-                                    firestore.collection('Rooms').snapshots(),
-                                builder: (context,
-                                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                                  List data = !snapshot.hasData
-                                      ? []
-                                      : snapshot.data!.docs
-                                          .where((element) => element['users']
-                                              .toString()
-                                              .contains(FirebaseAuth
-                                                  .instance.currentUser!.uid))
-                                          .toList();
-                                  return ListView.builder(
-                                    itemCount: data.length,
-                                    itemBuilder: (context, i) {
-                                      List users = data[i]['users'];
-                                      var friend = users.where((element) =>
-                                          element !=
-                                          FirebaseAuth
-                                              .instance.currentUser!.uid);
-                                      var user = friend.isNotEmpty
-                                          ? friend.first
-                                          : users
-                                              .where((element) =>
-                                                  element ==
-                                                  FirebaseAuth.instance
-                                                      .currentUser!.uid)
-                                              .first;
-                                      return FutureBuilder(
-                                          future: firestore
-                                              .collection('Users')
-                                              .doc(user)
-                                              .get(),
-                                          builder:
-                                              (context, AsyncSnapshot snap) {
-                                            return !snap.hasData
-                                                ? Container(
-                                                    child: Center(
-                                                      child: Text(''),
-                                                    ),
-                                                  )
-                                                : ChatWidgets.card(
-                                                    title: snap.data['name'],
-                                                    subtitle: data[i]
-                                                        ['last_message'],
-                                                    time: DateFormat('hh:mm a')
-                                                        .format(data[i][
-                                                                'last_message_time']
-                                                            .toDate()),
-                                                    onTap: () {
-                                                      Navigator.of(context)
-                                                          .push(
-                                                        MaterialPageRoute(
-                                                          builder: (context) {
-                                                            return ChatPage(
-                                                              id: user,
-                                                              name: snap
-                                                                  .data['name'],
-                                                            );
-                                                          },
-                                                        ),
-                                                      );
-                                                    },
-                                                  );
-                                          });
-                                    },
-                                  );
-                                }),
-                          ),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 20,
+                              spreadRadius: 2.0,
+                              offset: Offset(-10, 7)),
+                        ],
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.white,
+                            Colors.grey,
+                            const Color.fromARGB(255, 124, 123, 123)
+                          ],
                         ),
-                      ],
+                      ),
+                      margin: const EdgeInsets.only(top: 10),
+                      // decoration: Styles.friendsBox(),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              'Contacts',
+                              style: Styles.h1().copyWith(color: Colors.indigo),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12.0),
+                              child: StreamBuilder(
+                                  stream:
+                                      firestore.collection('Rooms').snapshots(),
+                                  builder: (context,
+                                      AsyncSnapshot<QuerySnapshot> snapshot) {
+                                    List data = !snapshot.hasData
+                                        ? []
+                                        : snapshot.data!.docs
+                                            .where((element) => element['users']
+                                                .toString()
+                                                .contains(FirebaseAuth
+                                                    .instance.currentUser!.uid))
+                                            .toList();
+                                    return ListView.builder(
+                                      itemCount: data.length,
+                                      itemBuilder: (context, i) {
+                                        List users = data[i]['users'];
+                                        var friend = users.where((element) =>
+                                            element !=
+                                            FirebaseAuth
+                                                .instance.currentUser!.uid);
+                                        var user = friend.isNotEmpty
+                                            ? friend.first
+                                            : users
+                                                .where((element) =>
+                                                    element ==
+                                                    FirebaseAuth.instance
+                                                        .currentUser!.uid)
+                                                .first;
+                                        return FutureBuilder(
+                                            future: firestore
+                                                .collection('Users')
+                                                .doc(user)
+                                                .get(),
+                                            builder:
+                                                (context, AsyncSnapshot snap) {
+                                              return !snap.hasData
+                                                  ? Container(
+                                                      child: Center(
+                                                        child: Text(''),
+                                                      ),
+                                                    )
+                                                  : ChatWidgets.card(
+                                                      title: snap.data['name'],
+                                                      subtitle: data[i]
+                                                          ['last_message'],
+                                                      time: DateFormat(
+                                                              'hh:mm a')
+                                                          .format(data[i][
+                                                                  'last_message_time']
+                                                              .toDate()),
+                                                      onTap: () {
+                                                        Navigator.of(context)
+                                                            .push(
+                                                          MaterialPageRoute(
+                                                            builder: (context) {
+                                                              return ChatPage(
+                                                                id: user,
+                                                                name: snap.data[
+                                                                    'name'],
+                                                              );
+                                                            },
+                                                          ),
+                                                        );
+                                                      },
+                                                    );
+                                            });
+                                      },
+                                    );
+                                  }),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
